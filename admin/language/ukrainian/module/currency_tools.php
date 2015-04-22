ea3e6d77413a53b13da0b4ebb64cc66a1452a124
JLMaksG <g_maks@ukr.net>
2015-04-22 16:55:15 +0300

    Create currency_tools.php

---
+++ admin/language/ukrainian/module/currency_tools.php
@@ -0,0 +1,80 @@
+<?php
+// Heading
+$_['heading_title']                     = 'Валютный Менеджер PRO v0.2.6.1.5 [by addist.ru]';
+$_['heading_general']                   = 'Основные данные';
+$_['heading_provider']                  = 'Настроки источника';
+$_['heading_payment']                   = 'Валюты способов оплаты';
+$_['heading_shipping']                  = 'Валюты способов доставки(исходняя валюта)';
+
+// Text
+$_['text_module']                       = 'Модули';
+$_['text_confirm']                      = 'Вы действительно хотите выполнить эту операцию?';
+$_['text_success']                      = 'Настройки успешно обновлены!';
+$_['text_updated']                      = 'Курсы валют успешно обновлены!';
+$_['text_converted']                    = 'Валюты товаров успешно сконвертированы!';
+$_['text_widget']                       = 'Виджет';
+$_['text_hours']                        = 'Часы';
+$_['text_name']                         = 'Название';
+$_['text_code']                         = 'Код';
+$_['text_value']                        = 'Значение';
+$_['text_country']                      = 'Страны';
+$_['text_payment']                      = 'Методы оплаты';
+$_['text_round']                        = 'Округление(-/+)';
+$_['text_correction']                   = 'Коррекция(-/+)';
+$_['text_margin']                       = 'Маржа(%)';
+$_['text_overheads']                    = 'Надбавка(-/+)';
+$_['text_update']                       = 'Обновить курсы';
+$_['text_action']                       = 'Действие';
+$_['text_edit']                         = 'Изменить';
+$_['text_currency']                     = 'Валюта';
+$_['text_by_category']                  = 'По разделам';
+$_['text_by_brand']                     = 'По брендам';
+
+// Tab
+$_['tab_general']                       = 'Основыне настройки';
+$_['tab_currency']                      = 'Валюты';
+$_['tab_widget']                        = 'Виджет';
+$_['tab_convert']                       = 'Конвертация';
+
+// Entry
+$_['entry_status']                      = 'Статус';
+$_['entry_update_auto']                 = 'Автоматическое обновление курсов';
+$_['entry_update_hours']                = 'Часы авто-обновления';
+$_['entry_currency_admin']              = 'Валюта админки';
+$_['entry_currency_admin_h']            = 'Валюту у которого булет курс равен к 1 и все операции системы будут уситываться в этой валюте.';
+$_['entry_currency_site']               = 'Валюта сайта по-умолчанию';
+$_['entry_currency_site_h']             = 'Валюта сайта по-умолчанию в которой вы принимаете заказы.';
+$_['entry_currency_site_strict']        = 'Запретить';
+$_['entry_currency_site_strict_h']      = 'Запретить смены валюты посетителями сайта.';
+$_['entry_currency_payment']            = 'Валюта оплаты по умолчанию';
+$_['entry_currency_payment_h']          = 'Валюта, в которой обычно принимаются платежи. В настройках ниже, можно настроить разные валюты для разных способов оплаты.';
+$_['entry_currency_second']             = 'Вторичная валюта';
+$_['entry_currency_second_h']           = 'Валюта которая показывается в скобках';
+$_['entry_provider']                    = 'Источник курса валют';
+$_['entry_provider_h']                  = 'Источник курсов валют, с которого вы берете курсы.';
+$_['entry_widget_currency']             = 'Валюта';
+$_['entry_widget_css_class']            = 'CSS клас сонтейнера';
+$_['entry_widget_template']             = 'Шаблон';
+$_['entry_widget_template_h']           = 'Переменные:<br />Коды валют в скобках([USD], [EUR], [GBP] ...)<br />{currency_main} - валюта виджета,<br />{currency} - текущая(выбранная или назначенная) валюта магазина,<br />{currency_admin} - валюта админки,<br />{currency_site} - валюта сайта по умолчанию,<br />{currency_second} - вторичная валюта,<br />{currency_payment} - валюта оплаты.</span>';
+$_['entry_convert_currency']            = 'Новая валюта';
+$_['entry_convert_skip']                = 'Пропустить товара с валютой';
+$_['entry_convert_criteria']            = 'Критерия';
+$_['entry_category']                    = 'Категории';
+$_['entry_brand']                       = 'Бренды';
+$_['entry_name']                        = 'Название';
+$_['entry_correction']                  = 'Коррекция(-/+)';
+$_['entry_margin']                      = 'Маржа(%)';
+$_['entry_overheads']                   = 'Надбавка(-/+)';
+$_['entry_round']                       = 'Значание округления(-/+)';
+$_['entry_auto_update']                 = 'Авто-обновление';
+$_['entry_country']                     = 'Страны';
+$_['entry_payment']                     = 'Способы оплаты';
+
+// Button
+$_['button_update']                     = 'Обновить курс';
+$_['button_convert']                    = 'Конвертировать';
+
+// Error
+$_['error_convert']                     = 'Не получилось конвертировать валюты товаров!';
+$_['error_update']                      = 'Не удается обновить курсы валют!';
+$_['error_convert_no_product']          = 'Не найден ни один товар для конвертации по заданным критериям!';
